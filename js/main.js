a8cb554ed3269bdecea82d7e77c8099a1db2ea18
ronny-318 <yjjung831@gmail.com>
2021-03-26 11:46:42 +0900

    주소창 반응형 수정
    js 추가
    css <section> height 추가

--- js/main.js
+++ js/main.js
@@ -15,3 +15,14 @@ $(function(){
         touchMove: false,
     });
 })
+
+// 브라우저 상단 주소 입력창 고려 반응형
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProgerty("--vh", `${vh}px`);
+
+window.addEventListener("resize", () => {
+    console.log("resize");
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProgerty("--vh", `${vh}px`);
+})
