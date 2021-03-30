ee1319f92db52ad32dd79c12738b7eda715c6b17
ronny-318 <yjjung831@gmail.com>
2021-03-30 15:23:11 +0900

    0330 update
    폰트 수정
    모바일 최적화 반응형 추가
    메인 화면 100vh 수정

--- js/main.js
+++ js/main.js
@@ -1,40 +1,30 @@
-$(function(){
-    $('.autoplay').slick({
-        infinite: true,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplayspeed: 3000,
-        prevArrow: false,
-        nextArrow: false,
-        draggable: false,
-        pauseOnFocus: false,
-        pauseOnHover: false,
-        swipe: false,
-        swipeToSlide: false,
-        touchMove: false,
-    });
+$(document).ready(function(){
+    $('#answer_btn').click(function(){
+        $('.question.first').hide();
+        $('#bar_gauge').css('width','16%');
+        $('.question.second').show();
+    })
 })
 
-// 브라우저 상단 주소 입력창 고려 반응형
+//브라우저 상단 주소 입력창 고려 반응형
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProgerty("--vh", `${vh}px`);
+//let vh = window.innerHeight * 0.01,
+//document.documentElement.style.setProgerty("--vh", `${vh}px`);
 
-window.addEventListener("resize", () => {
-    console.log("resize");
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProgerty("--vh", `${vh}px`);
-})
+//window.addEventListener("resize", () => {
+    //console.log("resize");
+    //let vh = window.innerHeight * 0.01;
+    //document.documentElement.style.setProgerty("--vh", `${vh}px`);
+//})
 
 
 // 카카오 링크 공유 API
-Kakao.init('83aeb588556f07e746244ef23e7f2dab');
+//Kakao.init('83aeb588556f07e746244ef23e7f2dab');
 
-function sendLink(){
-    Kakao.Link.createDefaultButton({
-        container: '.share',
-        objectType: 'feed',
-        content:{}
-    })
-}
+//function sendLink(){
+//    Kakao.Link.createDefaultButton({
+//        container: '.share',
+//        objectType: 'feed',
+//        content:{}
+//    })
+//}
